8352acb3c0374cc3a069af09821725ef87fdc2ab
Renukaeluri <eluri.renuka@gmail.com>
2016-01-12 01:00:24 -0800

    iupdates

---
+++ singly-linked-list.js
@@ -0,0 +1,40 @@
+var Node = function(value) {
+  var node = {};
+  node.value = value;
+  node.next = null;
+  return node;
+};
+
+var LinkedList = function() {
+  var list = {};
+  list.head = null;
+  list.tail = null;
+
+  list.addToTail = function(value) {
+    var node = Node(value);
+    if (!list.head) {
+      list.head = node;
+      list.tail = node;
+    } else {
+      list.tail.next = node;
+      list.tail = node;
+    }
+  };
+
+  list.removeHead = function() {
+    var oldHead = list.head;
+    list.head = oldHead.next;
+    return oldHead.value;
+  };
+
+  list.contains = function(target) {
+    var current = list.head;
+    while (current) {
+      if (current.value === target) return true;
+      current = current.next;
+    }
+    return false;
+  };
+
+  return list;
+};
